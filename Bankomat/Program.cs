b6907ade63d856dc6ab148a318c00f874454b7e1
Javohir0102 <javohiranvarov96@gmail.com>
2024-07-10 21:29:20 +0500

    Add project files.

---
+++ Bankomat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bankomat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AtmService service = new AtmService();
+        }
+    }
+}
